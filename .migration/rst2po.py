25b5aa7b8ffdea8de512fa73bbced94b7b1f7715
Manuel Kaufmann <humitos@gmail.com>
2020-05-02 14:30:35 +0200

    Initial working version of rst2po.py

--- .migration/rst2po.py
+++ .migration/rst2po.py
@@ -37,17 +37,19 @@ RST_ORIGINAL_DIR = os.path.abspath(
 def get_rst_file(pofilename):
     """Given a .po filename returns the corresponding .rst filename"""
     basename = os.path.basename(pofilename)
-    basename, ext = basename.split('.')
+    basename, ext = basename.rsplit('.', 1)
     rstfilename = os.path.join(RST_TRADUCIDOS_DIR, f'{basename}.rst')
     if os.path.exists(rstfilename):
         return rstfilename
 
 
 def get_rst_original_filename(rstfilename):
+    rst_original_filename = ''
     if rstfilename.endswith('real-index.rst'):
         rst_original_filename = 'index.rst'
 
-    rst_original_filename = os.path.join(RST_ORIGINAL_DIR, rst_original_filename)
+    basename = os.path.basename(rst_original_filename or rstfilename)
+    rst_original_filename = os.path.join(RST_ORIGINAL_DIR, basename)
     if os.path.exists(rst_original_filename):
         return rst_original_filename
 
@@ -55,23 +57,47 @@ def get_rst_original_filename(rstfilename):
 def create_english_spanish_sentences(rstfilename):
     """Create a tuple of (english, spanish) sentences for rstfilename"""
 
+    def get_paragraph(fd):
+        lines = []
+        paragraph = []
+        for line in fd.read().splitlines():
+            # import pdb; pdb.set_trace()
+            if any([
+                    line.startswith('.. '),
+                    line.startswith('==='),
+                    line.startswith('---'),
+                    line.startswith('***'),
+                ]):
+                continue
+
+            if line == '' and not paragraph:
+                continue
+
+            if line == '':
+                lines.append(' '.join(paragraph))
+                paragraph = []
+                continue
+            paragraph.append(line)
+
+        return lines
+
     # NOTE: we could use docutils and parse the rst in the correct way, but
     # that will probably take more time
+    with open(get_rst_original_filename(rstfilename)) as fd:
+        english = get_paragraph(fd)
 
     with open(rstfilename) as fd:
-        lines = []
-        for line in fd.read().splitlines():
-            if re.match('^[a-zA-Z] ', line):
-                # keep text lines only
-                lines.append(line)
-        # make the document just one line so we can split it in sentences
-        document = ' '.join(lines)
-        import pdb; pdb.set_trace()
+        spanish = get_paragraph(fd)
+
+    result = list(zip(english, spanish))
+    return result
 
 
-def get_rst_translation_text(rstfilename, text):
+def get_rst_translation_text(rstfilename, english_spanish, text):
     """Given an rstfilename an a text returns the corresponding translated text if exists"""
-    pass
+    for en, es in english_spanish:
+        if en == text:
+            return es
 
 
 def update_po_translation(pofilename, english, spanish):
@@ -84,7 +110,7 @@ for pofilename in glob.glob(PO_DIR + '**/*/*.po'):
     if rstfilename is None:
         continue
 
-    create_english_spanish_sentences(rstfilename)
+    english_spanish = create_english_spanish_sentences(rstfilename)
 
     po = polib.pofile(pofilename)
     for entry in po:
@@ -94,7 +120,7 @@ for pofilename in glob.glob(PO_DIR + '**/*/*.po'):
             # Do not override already translated text
             continue
 
-        translated_text = get_rst_translation_text(rstfilename, english_text)
+        translated_text = get_rst_translation_text(rstfilename, english_spanish, english_text)
         if translated_text is None:
             continue
 
