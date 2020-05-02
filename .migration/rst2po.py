8cbe9ed9feb4488b83a76afd875f3ef7ae55705f
Manuel Kaufmann <humitos@gmail.com>
2020-05-02 13:06:04 +0200

    About to start the (english, spanish) mapping list

--- .migration/rst2po.py
+++ .migration/rst2po.py
@@ -6,6 +6,7 @@
 # and there is no translation for that sentence/paragraph it updates the .po file with the translated text
 # from the .rst file.
 
+import re
 import glob
 import os
 import polib  # fades
@@ -17,17 +18,55 @@ PO_DIR = os.path.abspath(
         '..',
     ))
 
-RST_DIR = os.path.abspath(
+RST_TRADUCIDOS_DIR = os.path.abspath(
     os.path.join(
         os.path.dirname(__file__),
         'tutorialpyar',
+        'traducidos',
+    ))
+
+RST_ORIGINAL_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        'tutorialpyar',
+        'original',
     ))
 
 
 
 def get_rst_file(pofilename):
     """Given a .po filename returns the corresponding .rst filename"""
-    pass
+    basename = os.path.basename(pofilename)
+    basename, ext = basename.split('.')
+    rstfilename = os.path.join(RST_TRADUCIDOS_DIR, f'{basename}.rst')
+    if os.path.exists(rstfilename):
+        return rstfilename
+
+
+def get_rst_original_filename(rstfilename):
+    if rstfilename.endswith('real-index.rst'):
+        rst_original_filename = 'index.rst'
+
+    rst_original_filename = os.path.join(RST_ORIGINAL_DIR, rst_original_filename)
+    if os.path.exists(rst_original_filename):
+        return rst_original_filename
+
+
+def create_english_spanish_sentences(rstfilename):
+    """Create a tuple of (english, spanish) sentences for rstfilename"""
+
+    # NOTE: we could use docutils and parse the rst in the correct way, but
+    # that will probably take more time
+
+    with open(rstfilename) as fd:
+        lines = []
+        for line in fd.read().splitlines():
+            if re.match('^[a-zA-Z] ', line):
+                # keep text lines only
+                lines.append(line)
+        # make the document just one line so we can split it in sentences
+        document = ' '.join(lines)
+        import pdb; pdb.set_trace()
 
 
 def get_rst_translation_text(rstfilename, text):
@@ -40,11 +79,12 @@ def update_po_translation(pofilename, english, spanish):
     pass
 
 
-
-for pofilename in glob.glob(PO_DIR + '**/*.po'):
+for pofilename in glob.glob(PO_DIR + '**/*/*.po'):
     rstfilename = get_rst_file(pofilename)
-    if rst is None:
+    if rstfilename is None:
         continue
+
+    create_english_spanish_sentences(rstfilename)
 
     po = polib.pofile(pofilename)
     for entry in po:
@@ -58,4 +98,6 @@ for pofilename in glob.glob(PO_DIR + '**/*.po'):
         if translated_text is None:
             continue
 
-        update_po_translation(po, english_text, translated_text)
+        entry.msgstr = translated_text
+        # update_po_translation(po, english_text, translated_text)
+    po.save(pofilename)
