3951c61ca18a704ea1843efc147d407564228475
Manuel Kaufmann <humitos@gmail.com>
2020-05-02 11:01:06 +0200

    Skel for the rst2po.py script

--- .migration/rst2po.py
+++ .migration/rst2po.py
@@ -6,4 +6,56 @@
 # and there is no translation for that sentence/paragraph it updates the .po file with the translated text
 # from the .rst file.
 
+import glob
+import os
 import polib  # fades
+
+
+PO_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..',
+    ))
+
+RST_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        'tutorialpyar',
+    ))
+
+
+
+def get_rst_file(pofilename):
+    """Given a .po filename returns the corresponding .rst filename"""
+    pass
+
+
+def get_rst_translation_text(rstfilename, text):
+    """Given an rstfilename an a text returns the corresponding translated text if exists"""
+    pass
+
+
+def update_po_translation(pofilename, english, spanish):
+    """Update the pofilename with the translated spanish text"""
+    pass
+
+
+
+for pofilename in glob.glob(PO_DIR + '**/*.po'):
+    rstfilename = get_rst_file(pofilename)
+    if rst is None:
+        continue
+
+    po = polib.pofile(pofilename)
+    for entry in po:
+        english_text = entry.msgid
+        spanish_text = entry.msgstr
+        if spanish_text:
+            # Do not override already translated text
+            continue
+
+        translated_text = get_rst_translation_text(rstfilename, english_text)
+        if translated_text is None:
+            continue
+
+        update_po_translation(po, english_text, translated_text)
