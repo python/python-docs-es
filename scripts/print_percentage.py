46644c1a66790a25ee2356a6ef66f84486c3cc98
Manuel Kaufmann <humitos@gmail.com>
2020-05-06 14:20:24 +0200

    Adapt the percentage script to print per-file
    When an argument is passed, the script print percentage per-file.

--- scripts/print_percentage.py
+++ scripts/print_percentage.py
@@ -2,6 +2,7 @@
 
 import glob
 import os
+import sys
 
 import polib  # fades
 
@@ -12,11 +13,25 @@ PO_DIR = os.path.abspath(
     ))
 
 
+def get_percent_translated(pofilename):
+    po = polib.pofile(pofilename)
+    file_per = po.percent_translated()
+    return file_per
+
+
 def main():
-    for pofilename in glob.glob(PO_DIR + '**/tutorial/*.po'):
-        po = polib.pofile(pofilename)
-        file_per = po.percent_translated()
+    pofilename = None
+    if len(sys.argv) == 2:
+        pofilename = sys.argv[1]
+
+    if pofilename:
+        file_per = get_percent_translated(pofilename)
         print(f"{pofilename} ::: {file_per}%")
+
+    else:
+        for pofilename in glob.glob(PO_DIR + '**/tutorial/*.po'):
+            file_per = get_percent_translated(pofilename)
+            print(f"{pofilename} ::: {file_per}%")
 
 
 if __name__ == "__main__":
