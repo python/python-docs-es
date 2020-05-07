53c04f228cc9c6daf999703cd4363162151b73b4
Manuel Kaufmann <humitos@gmail.com>
2020-05-07 05:58:29 +0200

    Match PSF's official page

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ os.system('mkdir -p cpython/locales/es/')
 os.system('ln -nfs `pwd` cpython/locales/es/LC_MESSAGES')
 
 html_short_title = f'Documentación {release}'
-html_title = f'Documentación de Python en Español -- {version}'
+html_title = f'Documentación de Python en Español -- {release}'
 
 if not os.environ.get('SPHINX_GETTEXT') == 'True':
     # Override all the files from ``.overrides`` directory
