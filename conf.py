e55d799c61f9b22fbb7836ce6df9a8a7c1651c9d
Rodrigo Tobar <rtobar@icrar.org>
2024-11-22 14:33:24 +0800

    Remove unnecessary conf.py overrides
    These are already defined in cpython's, let's thus remove the amount of
    overriding we need to provide.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- conf.py
+++ conf.py
@@ -29,14 +29,7 @@ sys.path.append(os.path.abspath('cpython/Doc/includes'))
 sys.path.insert(0, os.path.abspath('cpython/Doc'))
 from conf import *
 
-# Call patchlevel with the proper path to get the version from
-# instead of hardcoding it
-from patchlevel import get_version_info
-version, release = get_version_info()
-
 project = 'Python en Español'
-year = time.strftime("%Y")
-copyright = f'2001-{year}, Python Software Foundation'
 
 html_theme_path = ['cpython/Doc/tools']
 templates_path = ['cpython/Doc/tools/templates']
